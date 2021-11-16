510a366fe55b46b0b501a1ad3b14b69c7314b00c
Oladipupo Fredrick <fredrickksola@gmail.com>
2021-11-16 10:40:40 +0100

    (feat): create migration,Model and controller  for casinos

---
+++ database/migrations/2021_11_16_091554_create_casinos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCasinosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('casinos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('logo_url')->nullable();
+            $table->longText('bonus_information')->nullable();
+            $table->string('affiliate_link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('casinos');
+    }
+}
